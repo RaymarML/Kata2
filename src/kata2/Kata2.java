5c4f990b68bca507dec1dee52f47548f639af21e
RaymarML <32089732+RaymarML@users.noreply.github.com>
2018-10-05 18:57:09 +0100

    Kata2 versión 5

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -5,14 +5,12 @@ import java.util.Map;
 public class Kata2 {
 
     public static void main(String[] args) {
-        Integer[] data = {2, 4, 4, 5, 6,
-                      4, 5, 3, 1, 6,
-                      8, 7, 7, 9, 9};
+        String[] data = {"Rosa", "Pepe", "María", "Pepe", "Pepe", "Rosa"};
         
         Histogram histo = new Histogram(data);
-        Map<Integer, Integer> histogr = histo.getHistogram();
+        Map<String, Integer> histogr = histo.getHistogram();
         
-        for(Map.Entry<Integer, Integer> entry : histogr.entrySet()){
+        for(Map.Entry<String, Integer> entry : histogr.entrySet()){
             System.out.println(entry.getKey() + " => " + entry.getValue());
         }
     }
